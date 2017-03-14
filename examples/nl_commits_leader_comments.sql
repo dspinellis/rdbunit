fd3f349a2e84fdb0ce2bff7ea7af1805e9bfc69c
Diomidis Spinellis <dds@aueb.gr>
2017-03-14 18:26:00 +0200

    Fix query
    Test passes

--- examples/nl_commits_leader_comments.sql
+++ examples/nl_commits_leader_comments.sql
@@ -1,20 +1,23 @@
 -- Commits by non-leaders commented by the leader
 
 create table leadership.nl_commits_leader_comments ENGINE=MyISAM AS
-  select project_leaders.project_id as project_id,
+  select project_commits.project_id as project_id,
     count(*) as n
 
-  from leadership.project_leaders
+  from (
+    select distinct project_leaders.project_id, yearly_commits.id
+    from leadership.project_leaders
 
-  left join leadership.yearly_commits
-  on yearly_commits.project_id = project_leaders.project_id
+    left join leadership.yearly_commits
+    on yearly_commits.project_id = project_leaders.project_id
 
-  left join leadership.yearly_commit_comments
-  on yearly_commits.id = yearly_commit_comments.commit_id
+    left join leadership.yearly_commit_comments
+    on yearly_commits.id = yearly_commit_comments.commit_id
 
-  where yearly_commit_comments.user_id = project_leaders.user_id and
-    yearly_commits.author_id != project_leaders.user_id
+    where yearly_commit_comments.user_id = project_leaders.user_id and
+      yearly_commits.author_id != project_leaders.user_id
+  ) as project_commits
 
-  group by project_leaders.project_id;
+  group by project_commits.project_id;
 
 alter table leadership.nl_commits_leader_comments add index(project_id);
