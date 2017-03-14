0bfea452ebac5c16aac8d6ba0435e73a0a44b4cb
Diomidis Spinellis <dds@aueb.gr>
2017-03-14 21:32:36 +0200

    Add number of commits with comments

---
+++ examples/commits_with_comments.sql
@@ -0,0 +1,21 @@
+-- Number of commits that have comments per project
+
+create table leadership.commits_with_comments ENGINE=MyISAM AS
+  select project_commits.project_id as project_id,
+    count(*) as n
+
+  from (
+    select distinct project_leaders.project_id, yearly_commits.id
+    from leadership.project_leaders
+
+    left join leadership.yearly_commits
+    on yearly_commits.project_id = project_leaders.project_id
+
+    inner join leadership.yearly_commit_comments
+    on yearly_commits.id = yearly_commit_comments.commit_id
+
+  ) as project_commits
+
+  group by project_commits.project_id;
+
+alter table leadership.commits_with_comments add index(project_id);
