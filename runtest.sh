d201dcbb58682512d09b1fdb11f2229fdb4a5ecf
Diomidis Spinellis <dds@aueb.gr>
2019-11-08 12:09:20 +0200

    Show failed run output

--- runtest.sh
+++ runtest.sh
@@ -10,6 +10,7 @@ for i ; do
   fi
   if ! sqlite3 <script.sql >script.out 2>&1 ; then
     echo "Sqlite execution failed" 1>&2
+    cat script.out 1>&2
     exit 1
   fi
   if egrep -v -e '^ok [0-9]' -e '^[0-9]+\.\.[0-9]+.?$' script.out ; then
